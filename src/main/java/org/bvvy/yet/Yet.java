cce08e72d8fc81c1a7672bb5f4995bc97ef350f6
bvvy <bvvy@qq.com>
2022-01-09 16:16:55 +0800

    feat: init project

---
+++ src/main/java/org/bvvy/yet/Yet.java
@@ -0,0 +1,9 @@
+package org.bvvy.yet;
+
+/**
+ * @author bvvy
+ */
+public class Yet {
+
+
+}
---
+++ src/test/java/org/bvvy/yet/TestYet.java
@@ -0,0 +1,15 @@
+package org.bvvy.yet;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bvvy
+ */
+public class TestYet {
+
+    @Test
+    public void test() {
+
+    }
+
+}
